25c90c0374ab740794e96d73a7ae29b208b11b6d
tszngai <ngaifu16@gmail.com>
2019-04-15 01:51:13 -0700

    saperate camera module and object dection module

--- src/camera/camera.js
+++ src/camera/camera.js
@@ -1,15 +1,10 @@
-'use strict';
-
-var _mobilenet = require('@tensorflow-models/mobilenet');
-
-var mobilenet = _interopRequireWildcard(_mobilenet);
-
 var NodeWebcam = require( "node-webcam" );
 
 var opts = {
   width: 1280,
   height: 720,
   quality: 100,
+  brightness: 20,
   delay: 0,
   saveShots: true,
   output: 'jpeg',
@@ -18,103 +13,10 @@ var opts = {
   verbose: false }
   
 var Webcam = NodeWebcam.create(opts);
-Webcam.capture( "../../img/capture.jpg", function( err, data ) {} );
 
-function _interopRequireWildcard(obj) {
-    if (obj && obj.__esModule) {
-        return obj;
-    } else {
-        var newObj = {};if (obj != null) {
-            for (var key in obj) {
-                if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key];
-            }
-        }newObj.default = obj;return newObj;
-    }
+async function capture() {
+	await Webcam.capture( "../../img/capture.jpg", function( err, data ) {} );
+	console.log('take');
 }
 
-var _rollupConfig = require('@tensorflow-models/coco-ssd');
-
-var _rollupConfig2 = _interopRequireDefault(_rollupConfig);
-
-global.fetch = require('node-fetch')
-
-function _interopRequireDefault(obj) {
-    return obj && obj.__esModule ? obj : { default: obj };
-}
-
-var cocoSsd = {};
-cocoSsd['RollupConfig'] = _rollupConfig2.default;
-
-var tf = require('@tensorflow/tfjs');
-
-require('@tensorflow/tfjs-node');
-
-var fs = require('fs');
-var jpeg = require('jpeg-js');
-
-var NUMBER_OF_CHANNELS = 3;
-
-var readImage = function readImage(path) {
-    var buf = fs.readFileSync(path);
-    var pixels = jpeg.decode(buf, true);
-    return pixels;
-};
-
-var imageByteArray = function imageByteArray(image, numChannels) {
-    var pixels = image.data;
-    var numPixels = image.width * image.height;
-    var values = new Int32Array(numPixels * numChannels);
-
-    for (var i = 0; i < numPixels; i++) {
-        for (var channel = 0; channel < numChannels; ++channel) {
-            values[i * numChannels + channel] = pixels[i * 4 + channel];
-        }
-    }
-
-    return values;
-};
-
-var imageToInput = function imageToInput(image, numChannels) {
-    var values = imageByteArray(image, numChannels);
-    var outShape = [image.height, image.width, numChannels];
-    var input = tf.tensor3d(values, outShape, 'int32');
-
-    return input;
-};
-
-var loadModel = async function loadModel(path) {
-    var mn = new mobilenet.MobileNet('1.00', '1.00');
-    mn.path = 'file://' + path;
-    await mn.load().catch(function (err) {
-	console.log('mn.load error!!!');
-        return console.log(err);
-    });
-    return mn;
-};
-
-var classify = async function classify(model, path) {
-    var image = readImage(path);
-    var input = imageToInput(image, NUMBER_OF_CHANNELS);
-
-    var mn_model = await loadModel(model);
-    var predictions = await mn_model.classify(input);
-
-    console.log('classification results:', predictions);
-    // console.log(predictions[0]['className']);
-    return [predictions[0]['className'], predictions[1]['className'], predictions[2]['className']];
-};
-
-if (process.argv.length !== 3) throw new Error('incorrect arguments: node script.js <MODEL> <IMAGE_FILE>');
-
-async function scan(){
-    var results = [];
-    while (!(process.argv[2] in results)){
-        await Webcam.capture( "../../img/capture.jpg", function( err, data ) {} );
-        results = await classify('model.json', '../../img/capture.jpg');
-    }
-    console.log('FOUND!!!!!!!!!!!!!!!!!!');
-}
-
-console.log('argv[2]');
-console.log(process.argv[2]);
-scan();
+setInterval(capture, 2000);
